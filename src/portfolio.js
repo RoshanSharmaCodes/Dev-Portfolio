036715f6ec32db2484c396902ece710ecde7e223
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-02-26 00:37:35 +0530

    * Added Twitter Embedded.

--- src/portfolio.js
+++ src/portfolio.js
@@ -385,7 +385,7 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "RoshanCodes_", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
